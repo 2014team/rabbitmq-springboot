ee2be3b6e1d7858b587a711bb8822bea6d14f2cb
zhuzeqing <645874679@qq.com>
2024-11-28 00:23:51 +0800

    init

--- src/test/java/com/zzq/rabbitmq/rabbitmqspringboot/producer/PublisherTest.java
+++ src/test/java/com/zzq/rabbitmq/rabbitmqspringboot/producer/PublisherTest.java
@@ -4,10 +4,15 @@ import com.zzq.rabbitmq.rabbitmqspringboot.config.RabbitMQConfig;
 import org.junit.jupiter.api.Test;
 import org.springframework.amqp.AmqpException;
 import org.springframework.amqp.core.Message;
+import org.springframework.amqp.core.MessageDeliveryMode;
 import org.springframework.amqp.core.MessagePostProcessor;
+import org.springframework.amqp.core.ReturnedMessage;
+import org.springframework.amqp.rabbit.connection.CorrelationData;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.io.IOException;
 
 /**
  * @author zzq
@@ -33,6 +38,56 @@ public class PublisherTest {
             @Override
             public Message postProcessMessage(Message message) throws AmqpException {
                 message.getMessageProperties().setCorrelationId("123");
+                return message;
+            }
+        });
+        System.out.println("消息发送成功");
+    }
+
+
+
+    @Test
+    public void publishWithConfirms() throws IOException {
+        rabbitTemplate.setConfirmCallback(new RabbitTemplate.ConfirmCallback() {
+            @Override
+            public void confirm(CorrelationData correlationData, boolean ack, String cause) {
+                if(ack){
+                    System.out.println("消息已经送达到交换机！！");
+                }else{
+                    System.out.println("消息没有送达到Exchange，需要做一些补偿操作！！retry！！！");
+                }
+            }
+        });
+        rabbitTemplate.convertAndSend(RabbitMQConfig.EXCHANGE,"big.black.dog","message");
+        System.out.println("消息发送成功");
+
+        System.in.read();
+    }
+
+    @Test
+    public void publishWithReturn() throws IOException {
+        // 新版本用 setReturnsCallback ，老版本用setReturnCallback
+        rabbitTemplate.setReturnsCallback(new RabbitTemplate.ReturnsCallback() {
+            @Override
+            public void returnedMessage(ReturnedMessage returned) {
+                String msg = new String(returned.getMessage().getBody());
+                System.out.println("消息：" + msg + "路由队列失败！！做补救操作！！");
+            }
+        });
+        rabbitTemplate.convertAndSend(RabbitMQConfig.EXCHANGE,"big.black.dog","message");
+        System.out.println("消息发送成功");
+
+        System.in.read();
+    }
+
+
+    @Test
+    public void publishWithBasicProperties() throws IOException {
+        rabbitTemplate.convertAndSend(RabbitMQConfig.EXCHANGE, "big.black.dog", "message", new MessagePostProcessor() {
+            @Override
+            public Message postProcessMessage(Message message) throws AmqpException {
+                // 设置消息的持久化！
+                message.getMessageProperties().setDeliveryMode(MessageDeliveryMode.PERSISTENT);
                 return message;
             }
         });
